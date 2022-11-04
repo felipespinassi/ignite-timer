85cb7c15c15a7415cd28a3a88014c973e186a6eb
felipespinassi <felipe_spinassi@hotmail.com>
2022-11-03 22:26:27 -0300

    adicionando estilod globais

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -15,14 +15,9 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme["green-500"]};
   color: ${(props) => props.theme.white};
   border: 0;
-  margin: 1px;
-
-  /* ${(props) => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `;
-  }} */
+  margin: 5px;
+  border-radius: 5px;
 `;
