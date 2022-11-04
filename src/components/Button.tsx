c8dd007ca4115dc51e52326914ab4133cabe455e
felipespinassi <felipe_spinassi@hotmail.com>
2022-11-03 22:17:39 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import styles from "./Button.module.css";
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+interface ButtonProps {
+  variant?: ButtonVariant;
+}
+
+export function Button({ variant = "primary" }: ButtonProps) {
+  return (
+    <>
+      <ButtonContainer variant={variant}>enviar</ButtonContainer>
+    </>
+  );
+}
