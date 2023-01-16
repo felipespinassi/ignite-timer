53b1df1ddc16abd787e37f4210e2afb45680260a
felipespinassi <felipe_spinassi@hotmail.com>
2023-01-15 22:37:20 -0300

    finalizando esilização e adicionando bibliotecas de formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,17 +8,39 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, " O ciclo precisa ser de no mínimo 5 minutos")
+    .max(60, " O ciclo precisa ser de no máximo 60 minutos"),
+});
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
             id="task"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -34,7 +56,7 @@ export function Home() {
             id="minutesAmount"
             step={5}
             min={5}
-            max={60}
+            {...register("minutesAmount, ", { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -47,7 +69,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} /> Começar{" "}
         </StartCountdownButton>
       </form>
